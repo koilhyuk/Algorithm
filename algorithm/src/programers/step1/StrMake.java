24b67d30e6ab06a0ce87eec0ee3fde5fad2d2db8
koilhyuk <dlfgur6203@naver.com>
2021-04-20 16:45:22 +0900

    test

--- algorithm/src/programers/step1/StrMake.java
+++ algorithm/src/programers/step1/StrMake.java
@@ -4,7 +4,7 @@ public class StrMake {
 	public static void main(String[] args) {
 		String s = "try helloworldjfdl awits asdwgeffa we fgfew fdsfas wqiewq mblxvf ";
 		String answer = "";
-		String[] temp = s.split(" ",-1);
+		String[] temp = s.split(" ", -1);
 		String ch = "";
 
 		for (int i = 0; i < temp.length; i++) {
@@ -16,7 +16,7 @@ public class StrMake {
 				answer += " ";
 			}
 		}
-		
+
 		System.err.println(answer);
 		System.err.println(answer.length());
 	}
