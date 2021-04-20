ef228b55bbdfb92a9a6918959f782f9f680cb1d4
koilhyuk <dlfgur6203@naver.com>
2021-04-21 08:44:26 +0900

    pokemon solution

---
+++ algorithm/src/programers/step1/Poketmon.java
@@ -0,0 +1,23 @@
+package programers.step1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Poketmon {
+	public static void main(String[] args) {
+
+		int[] nums = { 3,3,3,2,2,2};
+		int answer = 0;
+
+		int popNum = nums.length / 2;
+		Set<Integer> set = new HashSet<>();
+		for (int i : nums) {
+			set.add(i);
+		}
+		answer=(set.size()>=popNum?popNum:set.size());
+		
+		System.err.println(answer);
+
+	}
+
+}
