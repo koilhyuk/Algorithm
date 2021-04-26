9df8cdf3817750c4163138c5767365d2d47ec124
koilhyuk <dlfgur6203@naver.com>
2021-04-26 14:33:07 +0900

    add python solution

--- algorithm/src/programers/step1/Poketmon_1_1845.java
+++ algorithm/src/programers/step1/Poketmon_1_1845.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Poketmon_1_1845 {
 	public static void main(String[] args) {
 
-		int[] nums = { 3,3,3,2,2,2};
+		int[] nums = { 3, 3, 3, 2, 2, 2 };
 		int answer = 0;
 
 		int popNum = nums.length / 2;
@@ -14,10 +14,17 @@ public class Poketmon_1_1845 {
 		for (int i : nums) {
 			set.add(i);
 		}
-		answer=(set.size()>=popNum?popNum:set.size());
-		
+		answer = (set.size() >= popNum ? popNum : set.size());
+
 		System.err.println(answer);
 
 	}
 
 }
+// python
+//popNum=int(len(nums)/2)
+//tempSet=set(nums)
+//setLen=int(len(tempSet))
+//if setLen>=popNum:answer=popNum else:
+//	answer= setLen 
+//print(answer)
