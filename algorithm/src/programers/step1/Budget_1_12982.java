5ea7699111ebff4d4e312c2d8c2749c397bb014f
koilhyuk <dlfgur6203@naver.com>
2021-05-04 19:52:32 +0900

    add Budget python solution

--- algorithm/src/programers/step1/Budget_1_12982.java
+++ algorithm/src/programers/step1/Budget_1_12982.java
@@ -24,7 +24,13 @@ public class Budget_1_12982 {
 		}
 
 		System.out.println(answer);
-
 	}
-
 }
+// python solution
+//y = sorted(d, reverse=True)
+//while(len(y)):
+//    budget-=y.pop()
+//    if budget >= 0 :
+//        answer+=1
+//    else :
+//        break
