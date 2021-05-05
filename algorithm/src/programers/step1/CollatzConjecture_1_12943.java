1c79499a65881377295ef6334b7fbd67c155f1b5
koilhyuk <dlfgur6203@naver.com>
2021-05-05 22:23:47 +0900

    Add CollatzConjecture Python solution

--- algorithm/src/programers/step1/CollatzConjecture_1_12943.java
+++ algorithm/src/programers/step1/CollatzConjecture_1_12943.java
@@ -28,3 +28,13 @@ public class CollatzConjecture_1_12943 {
 		System.err.println(answer);
 	}
 }
+
+// python solution
+//while(num!=1):
+//    if(answer>500):
+//        return -1
+//        break
+//    answer+=1
+//    num = num*3+1 if num%2 else num/2
+//
+//return answer
