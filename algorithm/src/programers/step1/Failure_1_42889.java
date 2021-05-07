782f04d57248171a5d702e0a68956b111e8cefff
koilhyuk <dlfgur6203@naver.com>
2021-05-07 15:15:28 +0900

    Add Failure Python Solution

---
+++ algorithm/src/programers/step1/Failure_1_42889.java
@@ -0,0 +1,35 @@
+package programers.step1;
+
+public class Failure_1_42889 {
+
+}
+
+// python solution
+// 람다 이외의 방법 고민 ... doctionary 안에 담긴 값 정렬 하는 코드 고민.
+//
+//
+//testdiction ={}
+//stagelen =len(stages)
+//stages.sort()
+//print(stages)
+//for i in range(1, N+1):
+//    idx = 0
+//    cnt = stages.count(i)
+//    cal = 0.0 if not cnt else cnt/stagelen
+//    testdiction[i]=cal
+//    stagelen -= cnt
+//
+//# answer = sorted(testdiction, key = lambda x:  testdiction[x], reverse=True)
+//    
+//    for j in testdiction.keys():
+//        for i in answer:
+//            if testdiction.get(answer) > testdiction.get(j):
+//                idx+=1
+//            else:
+//                break
+//        answer.insert(idx, i)
+//
+//
+//# answer.reverse()
+//print(testdiction)
+//print(answer)
