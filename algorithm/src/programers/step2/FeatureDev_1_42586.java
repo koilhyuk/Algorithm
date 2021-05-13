3de36fb8059c130dcc94d6fd519ba03248e9b052
koilhyuk <dlfgur6203@naver.com>
2021-05-13 15:01:37 +0900

    add FeatureDev python solution

---
+++ algorithm/src/programers/step2/FeatureDev_1_42586.java
@@ -0,0 +1,34 @@
+package programers.step2;
+
+public class FeatureDev_1_42586 {
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
+// python solution
+//# progresses = [95, 90, 99, 99, 80, 99]
+//# speeds = [1, 1, 1, 1, 1, 1]
+//progresses = [5, 5, 5]
+//speeds = [21, 25, 20]
+//answer = []
+//restDay = [int((100-progresses[i])/speeds[i]) if (100-progresses[i]) % speeds[i]
+//           == 0 else int((100-progresses[i])/speeds[i]+1) for i in range(len(progresses))]
+//idx = 1
+//while len(restDay) > 0:
+//
+//    if(len(restDay)-1 >= idx and restDay[0]-restDay[idx] >= 0):
+//        idx += 1
+//    else:
+//        print(idx)
+//        print(restDay)
+//        answer.append(idx)
+//        restDay = restDay[idx:]
+//        idx = 1
+//
+//print(answer)
