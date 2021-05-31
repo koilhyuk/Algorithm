b6b4ce52e079ddb33c562cdd268376230f775c5b
koilhyuk <dlfgur6203@naver.com>
2021-06-01 00:23:28 +0900

    Add dwarf python solution

---
+++ algorithm/src/baekjoon/dwarf_1_2309.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+public class dwarf_1_2309 {
+
+	
+}
+
+// 난쟁이 python solution
+//list = [int(input()) for i in range(9)]
+//list.sort()
+//a=sum(list)-100
+//
+//for i in range(0,len(list)):
+//    for j in range(i+1,len(list)):
+//        if a == list[i]+list[j]:
+//            list.pop(j)
+//            list.pop(i)
+//            break
+//
+//for h in list:
+//    print(h)
