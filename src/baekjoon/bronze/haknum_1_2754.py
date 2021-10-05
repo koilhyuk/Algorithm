b2678e18fdc607b6d4e6a4258e9ef81d11189ebd
koilhyuk <dlfgur6203@naver.com>
2021-10-05 17:13:59 +0900

    Add haknum python solution

---
+++ src/baekjoon/bronze/haknum_1_2754.py
@@ -0,0 +1,19 @@
+score = input()
+result =0
+if score[:1] == "A":
+    result+=4
+elif score[:1] == "B":
+    result+=3
+elif score[:1] == "C":
+    result+=2
+elif score[:1] == "D":
+    result+=1
+
+if score[1:] =="+":
+    result+=0.3    
+elif score[1:] =="0":
+    result+=0.0
+elif score[1:] =="-":
+    result-=0.3
+
+print(result)
